151ccb53fa19a8fb734110e3457465e0c2a0e65d
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-06-14 01:01:28 +0300

    Refactor API method names for consistency

--- src/shulmanquotesapi.swift
+++ src/shulmanquotesapi.swift
@@ -21,6 +21,7 @@ extension URLSession {
 }
 
 // What an irony: I created this API and I am writing a wrapper for it. LMAO
+@MainActor
 public class Shulmanquotesapi{
     private let api = "https://shulmanquotes.vercel.app/api"
     private var headers: [String: String]
@@ -37,7 +38,7 @@ public class Shulmanquotesapi{
     }
     
     
-    public func get_api_info() async throws -> Any {
+    public func getApiInfo() async throws -> Any {
         let urlString = "\(api)/"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -49,7 +50,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func get_quote_by_id(quote_id: Int) async throws -> Any {
+    public func getQuoteById(quoteId: Int) async throws -> Any {
         let urlString = "\(api)/quote/\(quote_id)"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -61,7 +62,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func search_quote(q: String) async throws -> Any {
+    public func searchQuote(q: String) async throws -> Any {
         let urlString = "\(api)/search?q=\(q)"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -73,7 +74,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func get_random_quote() async throws -> Any {
+    public func getRandomQuote() async throws -> Any {
         let urlString = "\(api)/quote/random"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -85,7 +86,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func get_quote_list() async throws -> Any {
+    public func getQuoteList() async throws -> Any {
         let urlString = "\(api)/quotes"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -97,8 +98,8 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func get_episode_program_by_id(program: String,program_id: Int) async throws -> Any {
-        let urlString = "\(api)/episodes/\(program)/\(program_id)"
+    public func GetEpisodeProgramById(program: String,programId: Int) async throws -> Any {
+        let urlString = "\(api)/episodes/\(program)/\(programId)"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
         }
@@ -109,7 +110,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func get_random_episode(program: String) async throws -> Any {
+    public func GetRandomEpisode(program: String) async throws -> Any {
         let urlString = "\(api)/episodes/\(program)/random"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -122,7 +123,7 @@ public class Shulmanquotesapi{
     }
 
     
-    public func get_all_program_episodes(program: String) async throws -> Any {
+    public func getAllProgramEpisodes(program: String) async throws -> Any {
         let urlString = "\(api)/episodes/\(program)"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -134,7 +135,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
     
-    public func get_all_episodes() async throws -> Any {
+    public func getAllEpisodes() async throws -> Any {
         let urlString = "\(api)/episodes"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -146,7 +147,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
     
-    public func get_all_last_episodes() async throws -> Any {
+    public func getAllLastEpisodes() async throws -> Any {
         let urlString = "\(api)/last/episodes/all"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -158,7 +159,7 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
 
-    public func search_in_episode(q: String) async throws -> Any {
+    public func searchInEpisode(q: String) async throws -> Any {
         let urlString = "\(api)/search/episodes?q=\(q)"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
@@ -170,8 +171,32 @@ public class Shulmanquotesapi{
         return try JSONSerialization.jsonObject(with: data)
     }
     
-    public func get_last_episode(program: String) async throws -> Any {
+    public func getLastEpisode(program: String) async throws -> Any {
         let urlString = "\(api)/last/episodes/\(program)"
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+
+    public func getGithubAnalytics(username: String) async throws -> Any {
+        let urlString = "\(api)/github/\(username)/analytics"
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+    
+    public func getGithubLanguages(username: String) async throws -> Any {
+        let urlString = "\(api)/github/\(username)/"
         guard let url = URL(string: urlString) else {
             throw NSError(domain: "Invalid URL", code: -1)
         }
